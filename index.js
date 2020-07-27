bfc12eb10725c690789afe9fc95e85975b225285
Kirsten Lund <kirsten.d.lund@gmail.com>
2020-07-27 16:14:07 -0400

    updated port for glitch

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const { response } = require('express');
 
 const app = express();
 // const fetch = require('node-fetch');
-app.listen(3000, () => console.log('listening at 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Starting server at ${port}`));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
