2c3696f663bb866a9ebf873e93a59527001723f4
Kirsten Lund <kirsten.d.lund@gmail.com>
2020-07-27 16:51:48 -0400

    fixed backtick error

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { response } = require('express');
 const app = express();
 // const fetch = require('node-fetch');
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Starting server at ${port}'));
+app.listen(port, () => console.log(`Starting server at ${port}`));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
